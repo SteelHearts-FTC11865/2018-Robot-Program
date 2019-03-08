22a48cee96b5ba2e084a41501cb6a37bf733fca6
Steel Hearts <steelhearts4hclub@gmail.com>
2019-03-07 16:26:23 -0800

    Moved  old OpModes to ops.old and added @Disabled to them

---
+++ Program/TeamCode/src/main/java/com/steelhearts/main/ops/old/ArmOld.java
@@ -0,0 +1,23 @@
+package com.steelhearts.main.ops.old;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Disabled
+public class ArmOld extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        }
+    }
+
+}
---
+++ Program/TeamCode/src/main/java/com/steelhearts/main/ops/old/ViewCheck.java
@@ -0,0 +1,29 @@
+package com.steelhearts.main.ops.old;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="View Checker", group="Linear Opmode")
+@Disabled
+public class ViewCheck extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            TelemetryPacket packet = new TelemetryPacket();
+            packet.fieldOverlay()
+                    .setStrokeWidth(4)
+                    .setStroke("goldenrod")
+                    .strokeCircle(0, 0, 10);
+            dashboard.sendTelemetryPacket(packet);
+        }
+    }
+}
